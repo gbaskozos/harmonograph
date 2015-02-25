fe825fb1b1f8053137b75360864ed05d75d31b3c
Giorgos Baskozos <george.bask@gmail.com>
2015-02-25 12:47:21 +0000

    test2

--- harmonograph_function.R
+++ harmonograph_function.R
@@ -14,7 +14,7 @@ cat("calculating damping parameter d...\n")
 d1=runif(1,0,1e-02);d2=runif(1,0,1e-02);d3=runif(1,0,1e-02);d4=runif(1,0,1e-02)
 cat(" d1 = ", d1, "\n", "d2 = ", d2, "\n", "d3 = ", d3, "\n", "d4 = ", d4, "\n" )
 
-change 1
+change 1		change 2
 
 cat("calculating phase parameter p...\n")
 p1=runif(1,0,2*pi);p2=runif(1,0,2*pi);p3=runif(1,0,2*pi);p4=runif(1,0,2*pi)
